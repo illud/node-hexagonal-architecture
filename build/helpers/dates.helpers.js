30be73bf8e2edc492c511bd13000ff2044aae613
illud <61476419+saturnavt@users.noreply.github.com>
2023-07-25 17:47:29 -0500

    build remove

--- build/helpers/dates.helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateToYear = void 0;
-const dateToYear = (date) => {
-    const d = new Date(date);
-    let year = d.getFullYear();
-    return year;
-};
-exports.dateToYear = dateToYear;
